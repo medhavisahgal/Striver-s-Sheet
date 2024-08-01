c1f70cc1340562a102ba987f21c2cd80249f9cb0
medhavisahgal <112403556+medhavisahgal@users.noreply.github.com>
2024-08-01 17:27:23 +0530

    added one more problem

---
+++ pow.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    double myPow(double x, int n)
+    {
+        // BRUTE FORCE
+        long long int p = abs(n);
+        long double ans = 1;
+        // for(long long int i=1;i<=p;i++){
+        //     ans*=x;
+        // }
+        // if(n<0){
+        //     return 1.0/ans;
+        // }
+        // return ans;
+        // TLE-X=0.0001
+        // n=2147483647
+        // reason:Time complexity is O(n) which is pretty slow for large values of n.
+        // Binary Exponentiation
+        while (p)
+        {
+            if (p & 1)
+            {
+                ans *= x;
+            }
+            x *= x;
+            p >>= 1;
+        }
+        if (n < 0)
+        {
+            return 1.0 / ans;
+        }
+        return ans;
+    }
+};
+// Time complexity:O(log(n));
+// Space complexity:O(1);
