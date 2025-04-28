3b38b9012cc7bf41c73849ce98d72fef105d1473
medhavisahgal <18medhavi3418@gmail.com>
2025-04-28 17:31:43 +0530

    modified

--- Day-5/Middle_of_linkedlist.cpp
+++ Day-5/Middle_of_linkedlist.cpp
@@ -1,6 +1,6 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-//Definition for singly-linked list.
+// Definition for singly-linked list.
 struct ListNode
 {
     int val;
@@ -13,21 +13,37 @@ struct ListNode
 class Solution
 {
 public:
-    ListNode* middleNode(ListNode* head)
+    ListNode *middleNode(ListNode *head)
     {
-        ListNode *temp=head;
-        int num=0;
-        while(temp!=NULL)
+        // ListNode *temp=head;
+        // int num=0;
+        // while(temp!=NULL)
+        // {
+        //     num++;
+        //     temp=temp->next;
+        // }
+        // int mid=ceil(num/2);
+        // temp=head;
+        // while(mid--)
+        // {
+        //     temp=temp->next;
+        // }
+        // return temp;
+        // The above code is a brute force approach to find the middle node of a linked list.
+        // It first counts the number of nodes in the linked list, then calculates the middle index, and finally traverses the linked list again to reach the middle node.
+        // This approach has a time complexity of O(n) and a space complexity of O(1).
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast && fast->next)
         {
-            num++;
-            temp=temp->next;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        int mid=ceil(num/2);
-        temp=head;
-        while(mid--)
-        {
-            temp=temp->next;
-        }
-        return temp;
+        return slow;
+        // The slow pointer will be at the middle of the linked list when the fast pointer reaches the end.
+        // If the linked list has an even number of nodes, the slow pointer will be at the second middle node.
+        // If the linked list has an odd number of nodes, the slow pointer will be at the middle node.
+        // This is because the fast pointer moves twice as fast as the slow pointer.
+        // time complexity is O(n) and space complexity is O(1).
     }
 };
