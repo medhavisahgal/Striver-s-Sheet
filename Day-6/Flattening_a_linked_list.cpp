e3f2d03659bd2aaf6bdaacf028591c9fae7c0248
medhavisahgal <18medhavi3418@gmail.com>
2025-05-17 23:04:59 +0530

    modified

--- Day-6/Flattening_a_linked_list.cpp
+++ Day-6/Flattening_a_linked_list.cpp
@@ -8,43 +8,49 @@ struct Node
     Node *bottom;
     Node(int x) : data(x), next(nullptr), bottom(nullptr) {}
 };
-
+struct compare
+{
+    bool operator()(Node *a, Node *b)
+    {
+        return a->data > b->data;
+    }
+};
 class Solution
 {
 public:
-    Node *merge(Node *head1, Node *head2)
-    {
-        Node *dummy = new Node(0);
-        Node *tail = dummy;
-        while (head1 && head2)
-        {
-            if (head1->data < head2->data)
-            {
-                tail->bottom = head1;
-                head1 = head1->bottom;
-            }
-            else
-            {
-                tail->bottom = head2;
-                head2 = head2->bottom;
-            }
-            tail = tail->bottom;
-        }
-        if (head1)
-        {
-            tail->bottom = head1;
-        }
-        else
-        {
-            tail->bottom = head2;
-        }
-        return dummy->bottom;
-    }
+    // Node *merge(Node *head1, Node *head2)
+    // {
+    //     Node *dummy = new Node(0);
+    //     Node *tail = dummy;
+    //     while (head1 && head2)
+    //     {
+    //         if (head1->data < head2->data)
+    //         {
+    //             tail->bottom = head1;
+    //             head1 = head1->bottom;
+    //         }
+    //         else
+    //         {
+    //             tail->bottom = head2;
+    //             head2 = head2->bottom;
+    //         }
+    //         tail = tail->bottom;
+    //     }
+    //     if (head1)
+    //     {
+    //         tail->bottom = head1;
+    //     }
+    //     else
+    //     {
+    //         tail->bottom = head2;
+    //     }
+    //     return dummy->bottom;
+    // }
     // Function which returns the  root of the flattened linked list.
     Node *flatten(Node *root)
     {
-        if (!root || !root->next)
-            return root;
+        // if (!root || !root->next)
+        //     return root;
         // vector<int> v;
         // Node *temp = root;
         // while (temp)
@@ -78,10 +84,45 @@ public:
         // Time Complexity:O(n*mlog(n*m))
         // Space Complexity:O(n*m)
         // Recursive approach
-        root->next = flatten(root->next);
-        root = merge(root, root->next);
-        return root;
+        // root->next = flatten(root->next);
+        // root = merge(root, root->next);
+        // return root;
         // Time Complexity:O(n*m)
-        // Space Complexity:O(1)
+        // Space Complexity:O(n)
+        // Please note that the above code is not optimal and can be improved.
+        // The optimal solution is to use a priority queue or a min heap to merge the linked lists.
+        // This will reduce the time complexity to O(nlogk) where k is the number of linked lists.
+        // The space complexity will remain O(n) as we need to store the merged linked list.
+        // priority_queue<type, container, comparator> name;
+        priority_queue<Node *, vector<Node *>, compare> pq;
+        // Min-Heap
+        Node *head = nullptr;
+        Node *tail = nullptr;
+        while (root)
+        {
+            pq.push(root);
+            root = root->next;
+        }
+        while (!pq.empty())
+        {
+            auto minNode = pq.top();
+            pq.pop();
+            if (!head)
+            {
+                head = minNode;
+                tail = minNode;
+            }
+            else
+            {
+                tail->bottom = minNode;
+                tail = tail->bottom;
+            }
+            if (minNode->bottom)
+            {
+                pq.push(minNode->bottom);
+                minNode->bottom = nullptr;
+            }
+        }
+        return head;
     }
 };
